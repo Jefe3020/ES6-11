e4333e448289edafa400febd8bf1af46fb5b7874
Danny Thompson <flyhigh@FH-Systems.local>
2023-03-05 16:04:23 +0000

    new file

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+// const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "please write a comment about user",
+  },
 
+  // {questions is going to be an array of Objects}
+  //
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-
+  inquirer.prompt(questions).then((data) => {
+    console.log(data);
+  });
 }
 
 // function call to initialize program
